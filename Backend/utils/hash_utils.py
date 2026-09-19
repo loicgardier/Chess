ef50a63d3cb1695f349bfe0ff5fd62ff6dfe8eab
Loïc Gardier <loic.l.gardier@gmai.com>
2026-09-19 12:56:27 +0200

    fixing environment loading

--- Backend/utils/hash_utils.py
+++ Backend/utils/hash_utils.py
@@ -1,10 +1,11 @@
 import argon2
 from dotenv import load_dotenv,get_key
 load_dotenv()
+import os
 
-argon_hasher=argon2.PasswordHasher(time_cost=get_key('.env','ARGON2_TIME_COST'),
-                                   memory_cost=get_key('.env','ARGON2_MEMORYT'),
-                                   parallelism=get_key('.env','ARGON2_PARALLELISM'))
+argon_hasher=argon2.PasswordHasher(time_cost=int( os.environ["ARGON2_TIME_COST"]),
+                                   memory_cost=int(os.environ['ARGON2_MEMORYT']),
+                                   parallelism=int(os.environ['ARGON2_PARALLELISM']))
 
 def hash(password:str):
     return argon_hasher.hash(f'{password}{get_key('.env','PEPPER')}')
