45af49e7049a8fd65f9fb2f54f93f6c919cd658f
Loïc Gardier <loic.l.gardier@gmai.com>
2026-09-20 16:05:36 +0200

    add middleware for cors

--- Backend/server.py
+++ Backend/server.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from controlers import user_controler,tournament_controler
 import uvicorn
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app=FastAPI()
+
+app.add_middleware(
+CORSMiddleware,
+allow_origins=[os.environ['CLIENT_ORIGIN']], 
+allow_credentials=True, 
+allow_methods=["*"],
+allow_headers=["*"],
+)
+
 app.include_router(user_controler.user_router)
 app.include_router(tournament_controler.tournament_router)
 
