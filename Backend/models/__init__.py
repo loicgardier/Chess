f93b2a29f52cfbe151e6fe64a9d7cfef36623f5d
Loïc Gardier <loic.l.gardier@gmai.com>
2026-09-20 16:06:28 +0200

    add table refresh token

--- Backend/models/__init__.py
+++ Backend/models/__init__.py
@@ -5,5 +5,6 @@ from .inscriptions import Inscriptions
 from .categories import Categories
 from .rencontres import Rencontres
 from .tournaments_categories import TournamentsCategories
+from .refresh_token import RefreshToken
 
-__all__=["Base","Users","Tournaments","Inscriptions","Categories","Rencontres","TournamentsCategories"]
+__all__=["Base","Users","Tournaments","Inscriptions","Categories","Rencontres","TournamentsCategories","RefreshToken"]
